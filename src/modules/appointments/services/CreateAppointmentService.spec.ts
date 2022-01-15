2d3c952021c8a416ca4b7233c8a302bdf4924427
rsobreira93 <sobreiraromuloes@gmail.com>
2022-01-15 12:26:16 -0300

    feat: first test in appointments

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,20 @@
-test('sum two numberes', () => {
-  expect(1 + 2).toBe(3);
+import FakeAppointmentRepository from '../repositories/fakes/FakeAppointmentRespository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create new appointment', async () => {
+    const fakeAppointmentRepository = new FakeAppointmentRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentRepository
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123123',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123123');
+  });
+  //   it('should not be able to create new appointment on the same time', () => {});
 });
